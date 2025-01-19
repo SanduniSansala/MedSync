489a9f2398531f441920d18b4c870eda5719a1a7
SanduniSansala <karunathilakasanduni73@gmail.com>
2025-01-19 16:53:54 +0530

    Refactor createDoctor function to use axios instance for API calls

--- src/services/DoctorRoutes.ts
+++ src/services/DoctorRoutes.ts
@@ -1,14 +1,19 @@
 import axios from "axios";
 import { Doctor } from "../types/doctorTypes";
+import apiClient from "../axios/axios";
 
 const BASE_URL = "http://localhost:8080/Doctors";
 
-
-
 export const createDoctor = async (doctor: Doctor) => {
-    const response = await axios.post(`${BASE_URL}/create`, doctor);
-    return response.data;
-}
+
+    apiClient.post('/Docter/create', doctor).then((res) => {
+        return res.data;
+    }).catch((err) => {
+        console.log(err);
+    })}
+
+
+
 
 export const getDoctorById = async (id: string) => {
     const response = await axios.get<Doctor>(`${BASE_URL}/getById/${id}`);
