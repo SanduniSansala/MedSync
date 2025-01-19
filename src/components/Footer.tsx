49bd6144b5968f5a77de58c8745be51ead383b60
SanduniSansala <karunathilakasanduni73@gmail.com>
2025-01-20 05:19:23 +0530

    Refactor Footer component to use React Router Links and remove commented-out code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
   return (
@@ -8,73 +9,55 @@ const Footer: React.FC = () => {
           <div className="w-full md:w-1/3 mb-6">
             <h3 className="text-lg font-bold mb-4">SERVICES</h3>
             <ul className="space-y-2">
-              <li><a href="#" className="hover:underline  hover:text-blue-900">
-              Channel Your Doctor
-                </a></li>
-              <li><a href="#" className="hover:underline  hover:text-blue-900">
-              Ongoing Number
-                </a></li>
-              {/* <li><a href="#" className="hover:underline  hover:text-blue-900">
-              Audio/Video Consultation
-                </a></li>
-              <li><a href="#" className="hover:underline  hover:text-blue-900">
-              Medicine to Your Doorstep
-                </a></li>
-              <li><a href="#" className="hover:underline  hover:text-blue-900">
-              Healthcare to Your Doorstep
-                </a></li>
-              <li><a href="#" className="hover:underline  hover:text-blue-900">
-              Lab Reports at Your Fingertips
-                </a></li>
-              <li><a href="#" className="hover:underline  hover:text-blue-900">
-              Health Mart
-                </a></li>
-              <li><a href="#" className="hover:underline  hover:text-blue-900">
-              Vet Care by SLVA
-                </a></li> */}
+              <li>
+                <Link to="/DoctorChannel" className="hover:underline hover:text-blue-900">
+                  Channel Your Doctor
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className="hover:underline hover:text-blue-900">
+                  Ongoing Number
+                </Link>
+              </li>
             </ul>
           </div>
           <div className="w-full md:w-1/3 mb-6">
             <h3 className="text-lg font-bold mb-4">MedSync</h3>
             <ul className="space-y-2">
-              {/* <li><a href="#" className="hover:underline  hover:text-blue-900">
-              Partners
-                </a></li> */}
-              <li><a href="#" className="hover:underline  hover:text-blue-900">
-              About Us
-                </a></li>
-              {/* <li><a href="#" className="hover:underline hover:text-blue-900">
-              Terms & Conditions
-                </a></li>
-              <li><a href="#" className="hover:underline hover:text-blue-900">
-              Privacy Notice
-                </a></li> */}
-              <li><a href="#" className="hover:underline hover:text-blue-900">
-              Contact Us
-                </a></li>
-              <li><a href="#" className="hover:underline hover:text-blue-900">
-              FAQ
-                </a></li>
+              <li>
+                <Link to="/AboutUs" className="hover:underline hover:text-blue-900">
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link to="/ContactUs" className="hover:underline hover:text-blue-900">
+                  Contact Us
+                </Link>
+              </li>
+              <li>
+                <Link to="#" className="hover:underline hover:text-blue-900">
+                  FAQ
+                </Link>
+              </li>
             </ul>
           </div>
           <div className="w-full md:w-1/3 mb-6">
             <h3 className="text-lg font-bold mb-4">FOLLOW US</h3>
             <ul className="space-y-2">
               <li>
-                <a href="#" className="hover:underline  hover:text-blue-900">
+                <Link to="#" className="hover:underline hover:text-blue-900">
                   Facebook
-                </a>
+                </Link>
               </li>
-             
               <li>
-                <a href="#" className="hover:underline hover:text-blue-900">
-                  TouTube
-                </a>
+                <Link to="#" className="hover:underline hover:text-blue-900">
+                  YouTube
+                </Link>
               </li>
             </ul>
             <p className="text-xl">
-              <a href="tel:+94117990990" className="hover:underline text-red-500  hover:text-blue-900 ">
-                 +94 999 999 999
+              <a href="tel:+94117990990" className="hover:underline text-red-500 hover:text-blue-900">
+                +94 999 999 999
               </a>{" "}
               for Agent Assistance
             </p>
@@ -83,7 +66,9 @@ const Footer: React.FC = () => {
         <div className="border-t border-white mt-6 pt-6 text-center text-sm ">
           <p>© DIGITAL HEALTH PVT LTD. ALL RIGHTS RESERVED.</p>
           <p>
-            Technology Partner: <span className="font-bold">MedSync</span><br/><br/>
+            Technology Partner: <span className="font-bold">MedSync</span>
+            <br />
+            <br />
           </p>
         </div>
       </div>
