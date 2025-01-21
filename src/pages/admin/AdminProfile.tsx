0b952c761d6b26c397db8716cb3fd2780349bfc9
SanduniSansala <karunathilakasanduni73@gmail.com>
2025-01-21 12:23:07 +0530

    Fix import path for DoctorList component in AdminProfile

--- src/pages/admin/AdminProfile.tsx
+++ src/pages/admin/AdminProfile.tsx
@@ -2,7 +2,8 @@
 import React from 'react'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
-import DocterList from './DocterList'
+import DocterList from '../../components/DoctorList'
+
 
 const AdminProfile:React.FC = () => {
 
