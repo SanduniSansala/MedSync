4ce0cc973760090860b69872ce06e618e6a118bc
Tharushi-wijebandara <tharushiwijebandara715@gmail.com>
2025-01-22 20:49:23 +0530

    Fix formatting by removing unnecessary whitespace in DHome component

--- src/pages/Doctor/DHome.tsx
+++ src/pages/Doctor/DHome.tsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 
+
 const DHome:React.FC = () => {
   const navigate= useNavigate();
     const [isOpen, setIsOpen] = useState(false);
