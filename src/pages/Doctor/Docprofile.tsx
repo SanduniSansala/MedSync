8c68eb67593aa2c82cef829dcde9a89ccb43b6eb
Tharushi-wijebandara <tharushiwijebandara715@gmail.com>
2025-01-26 16:47:11 +0530

    Add useState import to Docprofile component

--- src/pages/Doctor/Docprofile.tsx
+++ src/pages/Doctor/Docprofile.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Dheader from '../../components/Dheader'
 import Footer from '../../components/Footer'
 import {LogIn , UserPlus} from 'lucide-react';
+import { useState } from 'react';
+
 
 export const Docprofile:React.FC = () => {
   return (
