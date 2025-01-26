4a182b5f8cb20265e5e5f03eadd3107a3ac46109
Tharushi-wijebandara <tharushiwijebandara715@gmail.com>
2025-01-26 17:02:48 +0530

    Add background image and layout structure to Docprofile component

--- src/pages/Doctor/Docprofile.tsx
+++ src/pages/Doctor/Docprofile.tsx
@@ -13,8 +13,17 @@ export const Docprofile:React.FC = () => {
     <div>
       <Dheader/>
       
+      <div>
+        
+           <main className="flex-grow flex items-center justify-center p-6 ">
+           <div
+      className="min-h-screen flex items-center justify-center bg-cover bg-center "
+      style={{ backgroundImage: `url('/images/doctor.jpeg')` }}
+    >
+           </div>
+            </main>  
+      </div>
       
-      
-      Docprofile</div>
+    </div>
   )
 }
