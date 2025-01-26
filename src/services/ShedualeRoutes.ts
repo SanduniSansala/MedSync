2321917e8c297f5869ddf13cfb8b6a5ad9ecff72
SanduniSansala <karunathilakasanduni73@gmail.com>
2025-01-26 22:03:57 +0530

    Refactor ShedualeRoutes to streamline update and delete operations

--- src/services/ShedualeRoutes.ts
+++ src/services/ShedualeRoutes.ts
@@ -3,28 +3,16 @@ import { Sheduale } from "../types/shedualeTypes";
 
 const BASE_URL = "http://localhost:8080/Sheduales";
 
-
+export const updateSheduale = async (id: string, sheduale: Sheduale) => {
+    const response = await axios.put(`${BASE_URL}/shedule/update/${id}`, sheduale);
+    return response.data;
+  } 
 export const createSheduale = async (sheduale: Sheduale) => {
     const response = await axios.post(`${BASE_URL}/create`, sheduale);
     return response.data;
 }
 
-export const getShedualeById = async (id: string) => {
-    const response = await axios.get<Sheduale>(`${BASE_URL}/getByID/${id}`);
-    return response.data;
-}
-
-export const getAllSheduales = async () => {
-    const response = await axios.get<Sheduale[]>(`${BASE_URL}/getAll`);
-    return response.data;
-}
-
-export const updateSheduale = async (id: string, sheduale: Sheduale) => {
-    const response = await axios.put<Sheduale>(`${BASE_URL}/update/${id}`, sheduale);
-    return response.data;
-}
-
 export const deleteSheduale = async (id: string) => {
-    const response = await axios.delete<string>(`${BASE_URL}/delete/${id}`);
-    return response.data;
-}
+  const response = await axios.delete(`${BASE_URL}/shedule/delete/${id}`);
+  return response.data;
+};
