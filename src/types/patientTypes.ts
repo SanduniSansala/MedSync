b2b9884d4dc6fd58bf629b91bce9cebde1417bb0
SanduniSansala <karunathilakasanduni73@gmail.com>
2025-01-26 21:14:43 +0530

    Fix Patient interface type definitions for consistency

--- src/types/patientTypes.ts
+++ src/types/patientTypes.ts
@@ -1,8 +1,8 @@
 export interface Patient{
-    id?: "string",
-    name: "string",
-    email: "stringuvuged",
-    password: "string",
-    nic: "string",
-    contactNo: "string"
+    id?: string,
+    name: string,
+    email: string,
+    password: string,
+    nic: string,
+    contactNo: string
   }
