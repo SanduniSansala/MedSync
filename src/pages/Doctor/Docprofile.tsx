e6af31536bf40bab241da64f8309659666ef9eac
Tharushi-wijebandara <tharushiwijebandara715@gmail.com>
2025-01-26 16:47:48 +0530

    Add useNavigate import to Docprofile component

--- src/pages/Doctor/Docprofile.tsx
+++ src/pages/Doctor/Docprofile.tsx
@@ -3,6 +3,7 @@ import Dheader from '../../components/Dheader'
 import Footer from '../../components/Footer'
 import {LogIn , UserPlus} from 'lucide-react';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 export const Docprofile:React.FC = () => {
