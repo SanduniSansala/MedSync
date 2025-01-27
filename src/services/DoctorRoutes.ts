f6f05935969512be559cbcc75729afe1e21a5e00
SanduniSansala <karunathilakasanduni73@gmail.com>
2025-01-28 03:23:12 +0530

    Refactor DoctorRoutes service for improved error handling and consistency

--- src/services/DoctorRoutes.ts
+++ src/services/DoctorRoutes.ts
@@ -1,36 +1,41 @@
-import axios from "axios";
-import { Doctor } from "../types/doctorTypes";
-import apiClient from "../axios/axios";
+import axios from 'axios';
 
-const BASE_URL = "http://localhost:8080/Doctors";
+const BASE_URL = 'http://localhost:8080/doctor';
+
+export interface Doctor {
+  doctorID: string;
+  name: string;
+  email: string;
+  contactNumber: string;
+  specialty: string;
+  password: string;
+}
 
 export const createDoctor = async (doctor: Doctor) => {
-
-    apiClient.post('/Docter/create', doctor).then((res) => {
-        return res.data;
-    }).catch((err) => {
-        console.log(err);
-    })}
-
-
-
+  try {
+    const response = await axios.post(`${BASE_URL}/create`, doctor);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const getDoctorById = async (id: string) => {
-    const response = await axios.get<Doctor>(`${BASE_URL}/getById/${id}`);
-    return response.data;
-}
+  const response = await axios.get<Doctor>(`${BASE_URL}/getById/${id}`);
+  return response.data;
+};
 
 export const getAllDoctors = async () => {
-    const response = await axios.get<Doctor[]>(`${BASE_URL}/getAll`);
-    return response.data;
-}
+  const response = await axios.get<Doctor[]>(`${BASE_URL}/getAll`);
+  return response.data;
+};
 
 export const updateDoctor = async (id: string, doctor: Doctor) => {
-    const response = await axios.put<Doctor>(`${BASE_URL}/update/${id}`, doctor);
-    return response.data;
-}
+  const response = await axios.put<Doctor>(`${BASE_URL}/update/${id}`, doctor);
+  return response.data;
+};
 
 export const deleteDoctor = async (id: string) => {
-    const response = await axios.delete<string>(`${BASE_URL}/delete/${id}`);
-    return response.data;
-}
+  const response = await axios.delete<string>(`${BASE_URL}/delete/${id}`);
+  return response.data;
+};
