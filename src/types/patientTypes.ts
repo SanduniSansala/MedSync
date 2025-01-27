4b52d178dedfa5a092a270162fd5c026cd781b01
SanduniSansala <karunathilakasanduni73@gmail.com>
2025-01-28 03:22:55 +0530

    Update Patient interface property names to follow PascalCase convention

--- src/types/patientTypes.ts
+++ src/types/patientTypes.ts
@@ -2,7 +2,7 @@ export interface Patient{
     id?: string;
     name: string;
     email: string;
-    nic: string;
-    password: string;
-    contactNo: string;
+    NIC: string;
+    Password: string;
+    ContactNo: string;
   }
