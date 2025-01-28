6f169db66e63d290ed8016cb051c680301b698e9
Tharushi-wijebandara <tharushiwijebandara715@gmail.com>
2025-01-28 08:08:49 +0530

    Remove unused home image and clean up layout in Home component

--- src/pages/Patient/Home.tsx
+++ src/pages/Patient/Home.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import homeimage from "../../assets/Images/Home.png";
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -12,13 +11,7 @@ const Home: React.FC = () => {
   return (
     <div>
       <Header />
-      
-
-      <div className="w-full flex justify-end h-auto items-end">
-        <div>
-          <img src={homeimage} alt="doctor-image" />
-        </div>
-      </div>
+    
       
       <Footer />
     </div>
