88604c8a33687de111dcc2e649e265ef48cb0e0f
SanduniSansala <karunathilakasanduni73@gmail.com>
2025-01-30 02:44:47 +0530

    Fix typo in doctorID field and update API calls to use apiClient in DoctorRoutes service

--- src/services/DoctorRoutes.ts
+++ src/services/DoctorRoutes.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import apiClient from '../axios/axios';
 
 const BASE_URL = 'http://localhost:8080/doctor';
 
 export interface Doctor {
-  doctorID: string;
+  docterID: string;
   name: string;
   email: string;
   contactNumber: string;
@@ -12,17 +13,22 @@ export interface Doctor {
 }
 
 export const createDoctor = async (doctor: Doctor) => {
-  try {
-    const response = await axios.post(`${BASE_URL}/create`, doctor);
-    return response.data;
-  } catch (err) {
+  apiClient.post('/Docter/create', doctor).then((res) => {
+    console.log(res.data);
+  }).catch((err) => {
     console.log(err);
-  }
+  })
 };
 
 export const getDoctorById = async (id: string) => {
-  const response = await axios.get<Doctor>(`${BASE_URL}/getById/${id}`);
-  return response.data;
+  apiClient.get(`/Docter/getById/${id}`).then((res) => {
+    console.log(res.data);
+  }).catch((err) => {
+    console.log(err);
+  })
+
+  // const response = await axios.get<Doctor>(`${BASE_URL}/getById/${id}`);
+  // return response.data;
 };
 
 export const getAllDoctors = async () => {
