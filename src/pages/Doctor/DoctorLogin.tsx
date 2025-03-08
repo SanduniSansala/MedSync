3e53648c313626f587da726fd8f1c3b4e951fb19
Tharushi-wijebandara <tharushiwijebandara715@gmail.com>
2025-03-08 17:31:09 +0530

    Refactor DoctorLogin component by removing unused import for cleaner code

--- src/pages/Doctor/DoctorLogin.tsx
+++ src/pages/Doctor/DoctorLogin.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Dheader from '../../components/Dheader';
 import Footer from '../../components/Footer';
