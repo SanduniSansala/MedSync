77ac635270113ca9393d8d250ba5c1033d76318a
SanduniSansala <karunathilakasanduni73@gmail.com>
2025-03-10 03:29:08 +0530

    Fix formatting in Sheduale interface by adding a newline at the end of the file

--- src/types/shedualeTypes.ts
+++ src/types/shedualeTypes.ts
@@ -1,7 +1,8 @@
 export interface Sheduale{
-      doctorID: string;
-      day: string; 
-      time: string;
-      count: number;
-      id?: string;
+    doctorID: string;
+    day: string; 
+    time: string;
+    count: number;
+    id?: string;
   }
+  
