5692dbf9426ea4eabc0e1ceb36879821111f882b
SanduniSansala <karunathilakasanduni73@gmail.com>
2025-03-09 19:05:05 +0530

    Add Dheader2 component for improved navigation and branding

---
+++ src/components/Dheader2.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import image from "../assets/images/Logo-Photoroom (1).png";
+import { useNavigate } from "react-router-dom";
+
+export const Dheader2:React.FC= () => {
+    const navigate = useNavigate();
+  return (
+    <div className="w-full flex justify-center bg-gradient-to-r from-blue-500 to-purple-500 items-center shadow-lg">
+      <div className="flex flex-row container bg-transparent py-4 px-6">
+        <div
+          className="basis-1/4 hover:cursor-pointer"
+          onClick={() => navigate("/Doctor")}
+        >
+          <img
+            className="flex items-center w-[130px] rounded-xl"
+            src={image}
+            alt="logo"
+          />
+        </div>
+        <div className="basis-2/4 flex gap-5 font-semibold cursor-pointer justify-center items-center text-white">
+          <div
+            className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-300 ease-in-out focus:outline-none"
+            onClick={() => navigate("/Doctor")}
+          >
+            Home
+          </div>
+          <div
+            className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 transition duration-300 ease-in-out focus:outline-none"
+            onClick={() => navigate("/DoctorLogin")}
+          >
+            Login
+          </div>
+          <div
+            className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition duration-300 ease-in-out focus:outline-none"
+            onClick={() => navigate("/Schedule")}
+          >
+            Schedule
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dheader2
