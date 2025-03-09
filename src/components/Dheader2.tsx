bb0887e0d71b9679af194a77af7688948bf0defc
SanduniSansala <karunathilakasanduni73@gmail.com>
2025-03-10 03:28:44 +0530

    Update navigation link in Dheader2 component to point to Schedule2

--- src/components/Dheader2.tsx
+++ src/components/Dheader2.tsx
@@ -32,7 +32,7 @@ export const Dheader2:React.FC= () => {
           </div>
           <div
             className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition duration-300 ease-in-out focus:outline-none"
-            onClick={() => navigate("/Schedule")}
+            onClick={() => navigate("/Schedule2")}
           >
             Schedule
           </div>
