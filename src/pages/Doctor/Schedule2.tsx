6876c5cdc9b68c7c7016910b98975a7824e3f80e
Tharushi-wijebandara <tharushiwijebandara715@gmail.com>
2025-03-09 18:47:38 +0530

    Fix formatting issue in Schedule2 component by removing extra newline

--- src/pages/Doctor/Schedule2.tsx
+++ src/pages/Doctor/Schedule2.tsx
@@ -10,8 +10,7 @@ const Schedule2 = () => {
   const timeSlots = [
     { id: 1, time: "09:00 AM", available: 10 },
     { id: 2, time: "10:00 AM", available: 8 },
-  
-  ];
+   ];
 
   return (
     <div>
