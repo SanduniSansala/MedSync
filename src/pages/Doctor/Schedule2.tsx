57ae6e1074b970529b94899c58dca4444cd487a5
Tharushi-wijebandara <tharushiwijebandara715@gmail.com>
2025-03-09 18:53:43 +0530

    Update Schedule2 component layout with responsive styling

--- src/pages/Doctor/Schedule2.tsx
+++ src/pages/Doctor/Schedule2.tsx
@@ -22,9 +22,10 @@ const Schedule2 = () => {
       setShowSuccess(true);
       setTimeout(() => setShowSuccess(false), 3000);
     }
+
   }
   return (
-    <div>
+    <div className="max-w-4xl mx-auto p-4">
 
     </div>
   )
