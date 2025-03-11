31fa4be2331e8fbe11d4ee6088579606f1dd5cd2
SanduniSansala <karunathilakasanduni73@gmail.com>
2025-03-11 16:25:55 +0530

    Add HadleSchedule component with header integration

---
+++ src/pages/Doctor/HadleSchedule.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Dheader from '../../components/Dheader'
+
+export const HadleSchedule:React.FC = () => {
+  return (
+    <div>
+        <Dheader/>
+
+    </div>
+  )
+}
+export default HadleSchedule;
