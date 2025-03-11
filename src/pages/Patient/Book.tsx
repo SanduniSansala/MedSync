761916cbad767e7eb25adf9287cc54bd8e08004b
SanduniSansala <karunathilakasanduni73@gmail.com>
2025-03-11 15:16:02 +0530

    Replace BookCard with BookCard2 in the Book component for enhanced scheduling functionality

--- src/pages/Patient/Book.tsx
+++ src/pages/Patient/Book.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
-import BookCard from '../../components/BookCard'
+import BookCard2 from '../../components/BookCard2'
 
 const Book:React.FC = () => {
   return (
     <div>
        <Header/>
       <div>
-        <BookCard/>
+        <BookCard2/>
       </div>
       <Footer/>
     </div>
